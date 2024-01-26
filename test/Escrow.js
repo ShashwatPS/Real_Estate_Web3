fa8a38177f3000604a2619341ec1f534647e8ce4
Shashwat Singh <shashwat123student@gmail.com>
2024-01-26 18:35:44 +0530

    Getting the Address

--- test/Escrow.js
+++ test/Escrow.js
@@ -2,9 +2,16 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 const tokens = (n) => {
-    return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+    return ethers.utils.parseUnits(n.toString(), 'ether');
+};
 
 describe('Escrow', () => {
+    it('saves the address', async () => {
+        const RealEstate = await ethers.getContractFactory('RealEstate');
+        const realEstate = await RealEstate.deploy();
 
-})
+        await realEstate.deployed();
+
+        console.log('RealEstate Contract Address:', realEstate.address);
+    });
+});

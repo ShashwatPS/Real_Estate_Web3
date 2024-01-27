2a847828e2a21dc881b0492fe48ecd74b6479845
Shashwat Singh <shashwat123student@gmail.com>
2024-01-27 14:40:14 +0530

    Organized tests

--- test/Escrow.js
+++ test/Escrow.js
@@ -9,8 +9,7 @@ describe('Escrow', () => {
     let buyer, seller, inspector, lender;
     let realEstate, escrow;
 
-    it('saves the address', async () => {
-
+    beforeEach(async () => {
         [buyer, seller, inspector, lender] = await ethers.getSigners()
 
         const RealEstate = await ethers.getContractFactory('RealEstate');
@@ -22,12 +21,29 @@ describe('Escrow', () => {
 
         const Escrow =  await ethers.getContractFactory('Escrow');
         escrow = await Escrow.deploy(realEstate.address, seller.address, inspector.address, lender.address);
+    })
 
-        let result = await escrow.nftAddress();
-        expect(result).to.equal(realEstate.address);
+    describe('Deployment', () => {
 
-        result = await escrow.seller();
-        expect(result).to.equal(seller.address);
+        it('Returns NFT Address', async () => {
+            const result = await escrow.nftAddress();
+            expect(result).to.equal(realEstate.address);
+        })
 
-    });
+        it('Returns Seller Address', async () => {
+            const result = await escrow.seller();
+            expect(result).to.equal(seller.address);
+        })
+
+        it('Returns Inspector Address', async () => {
+            const result = await escrow.inspector();
+            expect(result).to.equal(inspector.address);
+        })
+
+        it('Returns lender Address', async () => {
+            const result = await escrow.lender();
+            expect(result).to.equal(lender.address);
+        })
+
+    })
 });
